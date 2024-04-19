bedb97b42e49c673439016543617904d23c06dad
Jack Walker <119629209+jrw22@users.noreply.github.com>
2024-04-19 14:02:36 +0100

    Fix bug adding sentiment scores

--- analytics.py
+++ analytics.py
@@ -7,8 +7,9 @@ def get_sentiment(df: pd.DataFrame, text_column: str):
     # Initialise VADER
     sia = SentimentIntensityAnalyzer()
     # Apply VADER analysis on text column
-    df['sentiment_scores'] = df[text_column].apply(lambda x: sia.polarity_scores(x))
-    df['compound'] = df['sentiment_scores'].apply(lambda score_dict: score_dict['compound'])
-    df['sentiment'] = df['compound'].apply(lambda c: 'POSITIVE' if c >= 0.05 else ('NEGATIVE' if c <= -0.05 else 'NEUTRAL'))
+    sent_df=df.copy()
+    sent_df['sentiment_scores'] = sent_df[text_column].apply(lambda x: sia.polarity_scores(x))
+    sent_df['compound'] = sent_df['sentiment_scores'].apply(lambda score_dict: score_dict['compound'])
+    sent_df['sentiment'] = sent_df['compound'].apply(lambda c: 'POSITIVE' if c >= 0.05 else ('NEGATIVE' if c <= -0.05 else 'NEUTRAL'))
     
-    return df
+    return sent_df
