356849d8c200373b6653b14bc960ce46a2e102d3
Jack Walker <119629209+jrw22@users.noreply.github.com>
2024-08-15 15:29:06 +0100

    Final (submitted)

--- api.py
+++ api.py
@@ -1,19 +1,42 @@
 import praw
 import truststore
+import time
 
+class APIQueryCounter:
+    def __init__(self,):
+        self.query_count = 0
+        self.start_time = time.time()
+
+    def increment(self):
+        self.query_count =+ 1
+    
+    def get_count(self):
+        return self.query_count
+    
+    def reset(self):
+        self.query_count = 0
+        self.start_time = time.time()
+        
 class APIConnection:
     def __init__(self, client_id, client_secret, user_agent):
         self.client_id = client_id
         self.client_secret = client_secret
         self.user_agent = user_agent
+        self.api_counter = APIQueryCounter()
 
     def initialise_client(self):
-        
         truststore.inject_into_ssl()
-        
         reddit = praw.Reddit(
             client_id=self.client_id,
             client_secret=self.client_secret,
             user_agent=self.user_agent
         )
         return reddit
+    
+    def make_api_call(self, function, *args, **kwargs):
+        self.api_counter.increment()
+        return function(*args, **kwargs)
+    
+    def get_total_calls(self):
+        return self.api_counter.get_count()
+
